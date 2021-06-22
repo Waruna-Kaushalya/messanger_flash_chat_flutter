10066ab54f9188f28ee0d0bcfff478326ebd1dc2
Waruna-Kaushalya <waruna95610@gmail.com>
2021-06-22 11:11:02 +0530

    animated

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -13,26 +13,42 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
 
+  late Animation animation;
+
   @override
   void initState() {
     super.initState();
 
     controller = AnimationController(
+      duration: Duration(seconds: 1),
       vsync: this,
-      duration: Duration(
-        seconds: 1,
-      ),
     );
+
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(
+      controller,
+    );
+
     controller.forward();
+
     controller.addListener(() {
-      print(controller.value);
+      setState(() {});
+      print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red.withOpacity(controller.value),
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
